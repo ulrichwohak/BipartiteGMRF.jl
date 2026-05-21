e93c3b42cb69a3ae478b4d91beeef83f35c80ac4
Ulrich Wohak <ulrich@wohak.eu>
2026-05-21 10:42:43 +0200

    Stabilize JET and solver agreement tests

---
+++ test/test_jet.jl
@@ -0,0 +1,35 @@
+using JET
+
+function setup_jet_fixture()
+    problem = GMRFProblem(synthetic_df())
+    problem_values = Tuple(BipartiteGMRF.problem_fields(problem))
+    result = solve(
+        problem,
+        ExactCholesky(optim_iters=2, polish=false);
+        decompose=nothing,
+    )
+    return problem, problem_values, result
+end
+
+function jet_problem_flow(problem_values)
+    return GMRFProblem(problem_values...)
+end
+
+function jet_exact_solve_flow(problem)
+    Q = BipartiteGMRF.precision_matrix(problem, 0.3, 0.8, 0.6)
+    M = Q + (1.0 / 0.25^2) .* problem.VtV
+    return cholesky(Symmetric(M)) \ problem.projected_y
+end
+
+function jet_covariance_flow(result)
+    op = prior_covariance(result)
+    return cov_block(op; firms=(1,), workers=(10,))
+end
+
+@testset "JET" begin
+    problem, problem_values, result = setup_jet_fixture()
+
+    JET.@test_opt target_modules=(BipartiteGMRF,) jet_problem_flow(problem_values)
+    JET.@test_opt target_modules=(BipartiteGMRF,) jet_exact_solve_flow(problem)
+    JET.@test_opt target_modules=(BipartiteGMRF,) jet_covariance_flow(result)
+end
